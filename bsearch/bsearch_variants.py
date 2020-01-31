dbe531f5ca2ecf2b52fbf7deeef24ba24f5ed82e
gaotianpeng <gtpsydm@gmail.com>
2020-01-31 09:25:11 +0800

    finish the bsearch extensition

--- bsearch/bsearch_variants.py
+++ bsearch/bsearch_variants.py
@@ -1,6 +1,6 @@
 from typing import List
 # 查找第一个等于给定值的元素
-def bsearch_left_first(nums: List[int], target: int) -> int :
+def bsearch_first_equal(nums: List[int], target: int) -> int :
     low, high = 0, len(nums) - 1
     while low <= high:
         mid = low + ((high - low) >> 1)
@@ -15,21 +15,53 @@ def bsearch_left_first(nums: List[int], target: int) -> int :
                 high = mid - 1
 
 # 查找最后一个值等于给定值的元素
-def bsearch_right_last(nums: List[int], target: int) -> int:
-    # low, high = 0, len(nums) -1
-    pass
-#
+def bsearch_last_equal(nums: List[int], target: int) -> int:
+    low, high, n = 0, len(nums) - 1 , len(nums) - 1
+    while low <= high:
+        mid = low + ((high - low) >> 1)
+        if nums[mid] < target:
+            low = mid + 1
+        elif nums[mid] > target:
+            high = mid - 1
+        else:
+            if mid == n or nums[mid+1] != target:
+                return mid
+            else:
+                low = mid + 1
+    return -1
 
 # 查找第一个大于等于给定值的元素
-def bsearch_left_not_less():
-    pass
+def bsearch_first_not_less(nums: List[int], target) -> int:
+    low, high = 0, len(nums) - 1
+    while low <= high:
+        mid = low + ((high - low) >> 1)
+        if nums[mid] >= target:
+            if mid == 0 or nums[mid-1] < target:
+                return mid
+            else:
+                high = mid - 1
+        else:
+            low = mid + 1
+    return -1
 
 # 查找最后一个小于等于给定值的元素
-def bsearch_right_not_greater():
-    pass
-
+def bsearch_less_not_greater(nums: List[int], target: int):
+    low, high , n = 0, len(nums) - 1, len(nums) - 1
+    while low <= high:
+        mid = low + ((high - low) >> 1)
+        if nums[mid] > target:
+            high = mid - 1
+        else:
+            if mid == n or nums[mid+1] > target:
+                return mid
+            else:
+                low = mid + 1
+    return -1
 
 ###############################################
 ## test
 test_data = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 9]
-print(bsearch_left_first(test_data, 5))
+print(bsearch_first_equal(test_data, 5))
+print(bsearch_last_equal(test_data, 5))
+print(bsearch_first_not_less(test_data, 7))
+print(bsearch_less_not_greater(test_data, 8))
