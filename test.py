4529827e07d576653a8aca5dc033e57c34e80275
gaotianpeng <gtpsydm@gmail.com>
2020-02-01 17:38:23 +0800

    heap

--- test.py
+++ test.py
@@ -1,12 +1,21 @@
-class Person(object):
-    def __init__(self, name, gender):
-        self.name = name
-        self.gender = gender
+# def my_decorator(func):
+#     def wrapper():
+#         print('wrapper of decorator')
+#         func()
+#     return wrapper
+#
+# def greet():
+#     print('hello world')
+#
+# greet = my_decorator(greet)
+# greet()
+def my_decorator(func):
+    def wrapper():
+        print('wrapper of decorator')
+        func()
+    return wrapper
+@my_decorator
+def greet():
+    print('hello world')
 
-    def __str__(self):
-        return '(Person: %s, %s)' % (self.name, self.gender)
-
-    __repr__ = __str__
-
-p = Person('Bob', 'male')
-p
+greet()
